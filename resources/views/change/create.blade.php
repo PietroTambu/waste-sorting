c7c69020dcb29ddbe5883cf8509c2e7f34fa9f48
Tambu <pietro.tambu2003@gmail.com>
2021-08-02 15:46:26 +0200

    02-08-2021

--- resources/views/change/create.blade.php
+++ resources/views/change/create.blade.php
@@ -1,43 +1,43 @@
 <?php include(app_path().'/includes/header.php'); ?>
 <?php include(app_path().'/includes/navbar.php'); ?>
-    <h1>Add Collection Event</h1>
+<h1 class="text-center">Add Collection Event</h1>
 
-    <form action="/change/create" method="POST">
-        {{ csrf_field() }}
-        <p>
-            <label for="garbage">Choose the type:</label>
-            <input type="text" name="name" autocomplete="off" list="garbagelist" placeholder="Select or type the garbage typology" required>
-            <datalist id="garbagelist">
-                <option value="Indifferenziato">
-                <option value="Vetro">
-                <option value="Plastica">
-                <option value="Umido">
-                <option value="Carta">
-            </datalist>
-        </p>
-        <p>
-            <label for="day">Choose a day:</label>
-            <select id="day" name="day" required>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-                <option value="Saturday">Saturday</option>
-                <option value="Sunday">Sunday</option>
-            </select>
-        </p>
-        <p>
-            <label for="start">Choose the starting time:</label>
-            <input type="time" name="start" required>
-        </p>
-        <p>
-            <label for="end">Choose the end time:</label>
-            <input type="time" name="end" required>
-        </p>
-        <p>
-        <button type="submit">Add Collection Event</button>
-        </p>
-        
-    </form>
+<form action="/change/create" method="POST" class="text-center">
+    {{ csrf_field() }}
+    <div class="form-group w-25 m-auto">
+        <label for="garbage">Choose the type:</label>
+        <input type="text" class="form-control text-center" name="name" autocomplete="off" list="garbagelist" placeholder="Select or type the garbage typology" required>
+        <datalist id="garbagelist">
+            <option>Indifferenziato</option>
+            <option>Vetro</option>
+            <option>Plastica</option>
+            <option>umido</option>
+            <option>Carta</option>
+        </datalist>
+    </div>
+    <div class="form-group w-25 m-auto">
+        <label for="day">Choose a day:</label>
+        <select class="form-control text-center" id="day" name="day" required>
+            <option>Monday</option>
+            <option>Tuesday</option>
+            <option>Wednesday</option>
+            <option>Thursday</option>
+            <option >Friday</option>
+            <option>Saturday</option>
+            <option>Sunday</option>
+        </select>
+    </div>
+    <div class="form-group w-25 m-auto">
+        <label for="start">Choose the starting time:</label>
+        <input class="form-control text-center" type="time" name="start" required>
+    </div>
+    <div class="form-group w-25 m-auto">
+        <label for="end">Choose the end time:</label>
+        <input class="form-control text-center" type="time" name="end" required>
+    </div>
+    <div>
+        <button type="submit" class="btn btn-primary my-3">Add Collection Event</button>
+    </div>
+
+</form>
 <?php include(app_path().'/includes/footer.php'); ?>
