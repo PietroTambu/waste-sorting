a0e4241bf2cdcf2bd2b3e2e01e19e88bb559e32e
Tambu <pietro.tambu2003@gmail.com>
2021-08-02 18:44:37 +0200

    02-08-2021 18:44

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,12 @@
 <?php include(app_path().'/include/navbar.php'); ?>
 
     <h1 class="text-center">Welcome to Waste Sorting Planning</h1>
+    <hr>
+    <div class="text-center">
+        <h2>To start push in the database the example events by clicking <a href="/loadExampleData" class="text-center">here</a></h2>
+        <h3>Otherwhise you can insert manually by the <i><a href="/change/create">new collection</a></i> route</h3>
 
-    <a href="/loadExampleData" class="text-center">Load Example Data</a>
+    </div>
+
 
 <?php include(app_path().'/include/footer.php'); ?>
